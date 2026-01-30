cf7b120be28c571e59185fbfdb31026a7d8214aa
strikeitbroke <strikeitbroke@gmail.com>
2026-01-29 22:15:59 -0800

    #3 Add the strava user model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+
+from core import settings
 
 
 class User(models.Model):
@@ -11,3 +14,28 @@ class User(models.Model):
 
     def __str__(self):
         return f"<{self.email}>"
+
+
+class UserStrava(models.Model):
+    athlete_id = models.CharField(max_length=255, unique=True)
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
+    expires_at = models.BigIntegerField()  # Stores the POSIX timestamp
+    scope: models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def check_and_refresh(self, client):
+        # Use a 5-minute (300s) buffer for safety
+        if timezone.now().timestamp() + 300 >= self.expires_at:
+            res = client.refresh_access_token(
+                client_id=settings.MY_STRAVA_CLIENT_ID,
+                client_secret=settings.MY_STRAVA_CLIENT_SECRET,
+                refresh_token=self.refresh_token,
+            )
+            self.access_token = res["access_token"]
+            self.refresh_token = res["refresh_token"]
+            self.expires_at = res["expires_at"]
+            self.save()
+            return True  # Refreshed
+        return False  # Still valid
