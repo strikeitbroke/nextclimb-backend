982221aa6ec611bc98133446bcdd1558212a8642
strikeitbroke <strikeitbroke@gmail.com>
2026-03-01 20:04:07 -0800

    load secret from env

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env.local"), override=True)
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4hynm5qtur-@zz&)!ipjm44dbr_o@_+z6)rz+$2j!&#ib2x^x("
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
