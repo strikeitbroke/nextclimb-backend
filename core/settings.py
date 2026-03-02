ab161332e78ed07608ef39b2810adab4fce0e476
strikeitbroke <strikeitbroke@gmail.com>
2026-03-01 18:29:34 -0800

    collecting static files

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # custom settings
@@ -139,6 +140,11 @@ MY_STRAVA_CLIENT_SECRET = os.getenv("MY_STRAVA_CLIENT_SECRET")
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://nextclimb.fit",
+    "https://www.nextclimb.fit",
 ]
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 # Run migration
 python manage.py migrate
 
+# Collect static files for Django admin
+python manage.py collectstatic --noinput
+
 python manage.py runserver 0.0.0.0:8000
